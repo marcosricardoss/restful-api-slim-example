ad4386a97e226e057774e31bc2af4526e91435b7
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-14 14:07:50 -0300

    Creation of the method responsible for the user login in the AuthController.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,6 +7,31 @@ use Marcosricardoss\Restful\Helpers;
 
 final class AuthController {
 
+      /**
+     * Login a user.
+     *
+     * @param Slim\Http\Request  $request
+     * @param Slim\Http\Response $response
+     *
+     * @return Slim\Http\Response
+     */
+    public function login($request, $response) {
+        
+        $userData = $request->getParsedBody();
+        
+        if ($this->validateUserData($userData)) {
+            return $response->withJson(['message' => 'Username or Password field not provided.'], 400);
+        }               
+        
+        $user = User::where('username', $userData['username'])->first();
+
+        if (!password_verify($userData['password'], $user->password)) {
+            return $response->withJson(['message' => 'Username or Password not valid.'], 401);
+        }
+        
+        return $response->withJson(['message' => 'The user is logged in.'], 201);
+    }
+
      /**
      * Register a user.
      *
