503e7b49ff70a6a30fbcdd3afd3e112db2296119
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-13 22:54:15 -0300

    Creation of the class 'DatabaseSchema', responsible for creating a table in the database and creating the method responsible for creating the user table.

---
+++ src/DatabaseSchema.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Marcosricardoss\RestAPI;;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
+
+class DatabaseSchema {
+    
+    /**
+     * Create needed tables in database.
+     */
+    public static function createTables() {
+        self::createUsersTable();        
+    }
+
+    private static function createUsersTable() {
+        if (!Capsule::schema()->hasTable('users')) {
+            Capsule::schema()->create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('username');
+                $table->string('role');
+                $table->string('password');
+            });
+        }
+    }
+    
+}
