d217c61803f78acd0b80ac650ddc0148d245954d
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-14 14:09:41 -0300

    Creation of the route to the endpoint /auth/login, responsible for logging in the user.

--- src/routes.php
+++ src/routes.php
@@ -11,5 +11,9 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 });
 
 $app->group('/auth', function () {   
+    
+    $this->post('/login', "Marcosricardoss\Restful\Controller\AuthController:login");
     $this->post('/register', "Marcosricardoss\Restful\Controller\AuthController:register");
+    
+
 });
