58db75971e45be40bb6355772d992b8a60e6e6a9
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-25 11:58:46 -0300

    Creation of the class TestDatabasePopulator, responsible for adding in the database the content to be used in the tests.

---
+++ tests/Functional/TestDatabasePopulator.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+use Marcosricardoss\Restful\User;
+
+class TestDatabasePopulator {
+    
+    /**
+     * Populate test Database with tests values.
+     *
+     * @return Pyjac\NaijaEmoji\Model\User
+     */
+    public static function populate()
+    {
+        Capsule::beginTransaction();
+        try {
+            $user = User::firstOrCreate(['username' => 'tester', 'password' => password_hash('test', PASSWORD_DEFAULT), 'role' => 'member']);
+            # all good
+        } catch (\Exception $e) {
+            Capsule::rollback();
+            throw $e;
+            # something went wrong
+        }
+        
+        # create user
+        return $user;
+    }
+}
