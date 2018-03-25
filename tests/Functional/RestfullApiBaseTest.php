9bc77bbdbc8f0e8e7b72db8e968a20a5cae869f8
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-25 19:02:04 -0300

    [Chore] Add new test case for Not Found Route

--- tests/Functional/RestfullApiBaseTest.php
+++ tests/Functional/RestfullApiBaseTest.php
@@ -3,6 +3,10 @@ require_once 'RestfulApiTest.php';
 
 class RestfulApiBaseTest extends RestfulApiTest {
     
-    
+    public function testGetReturnsStatusCode404WithMsgWhenRequestRouteDoesNotExit() {
+        $response = $this->get('/xxxxxxx');
+        $data = json_decode($response->getBody(), true);
+        $this->assertSame($response->getStatusCode(), 404);
+    }    
     
 }
