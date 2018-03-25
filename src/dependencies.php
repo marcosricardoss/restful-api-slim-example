b72813d9cbeb3fd11e9eb459cced15e774e1312b
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-25 11:37:03 -0300

    Creation in the dependence.php file, a custom notFoundHandler, to deal with not found routes

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,6 +4,17 @@
 
 $container = $app->getContainer();
 
+# custom notFoundHandler to deal with no found routes
+$container['notFoundHandler'] = function ($container) {
+    return function ($request, $response) use ($container) {
+         $r = $response
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Page not found');
+        return $r;
+    };
+};
+
 # view renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
