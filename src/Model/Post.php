af8f7071e4c0f21cd554442a2e4e63f5d3b96f22
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-28 21:48:57 -0300

    [Chore] Added in the Post model, a method to get the post creator

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Post extends Eloquent {    
     
+    /**
+     * Get the creator of the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function created_by() {
+        return $this->belongsTo('Marcosricardoss\Restful\Model\User', 'created_by');
+    }
 }
