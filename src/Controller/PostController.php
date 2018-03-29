56159ec3711cc0f61b18e0932b21e41b3b67ba86
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-28 23:01:41 -0300

    [Chore] Method created to the route of post creating, to save the post in database and to validate the post data.

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,66 @@
 
 namespace Marcosricardoss\Restful\Controller;
 
+use Illuminate\Database\Capsule\Manager;
+use Marcosricardoss\Restful\Helpers;
+use Marcosricardoss\Restful\Model\User;
+use Marcosricardoss\Restful\Model\Post;
+use Marcosricardoss\Restful\Model\Category;
+
 final class PostController {
 
+    /**
+     * Route for creating a post.
+     *
+     * @param Slim\Http\Request  $request
+     * @param Slim\Http\Response $response
+     * @param array              $args
+     *
+     * @return Slim\Http\Response
+     */
+    public function create($request, $response, $args)
+    {
+        $postData = $request->getParsedBody();
+        if (!$postData || !$this->requiredPostDataAreProvided($postData)) {
+            throw new \UnexpectedValueException('The supplied post data is not formatted correctly.');
+        }        
+        $user = $request->getAttribute('user');
+        $this->createPost($postData, $user);
+        
+        return $response->withJson(['message' => 'Post created successfully.'], 201);
+    }
+
+    /**
+     * Create a post in the database.
+     *
+     * @param array                              $postData
+     * @param Marcosricardoss\Restful\Model\User $user
+     *
+     * @return void
+     */
+    private function createPost($postData, $user) {
+        Manager::transaction(function () use ($postData, $user) {
+            $category = Category::firstOrCreate(['name' => $postData['category']]);
+            $post = new Post();
+            $post->title = $postData['title'];
+            $post->content = $postData['content'];
+            $post->category_id = $category->id;
+            $user->posts()->save($post);            
+        });
+    }
+
+    /**
+     * * Validating post data.
+     *
+     * @param array $postData
+     *
+     * @return bool
+     */
+    private function requiredPostDataAreProvided($postData) {
+        $requiredStrings = ['title','category'];
+        if (!Helpers::keysExistAndNotEmptyString($requiredStrings, $postData)) {
+            return false;
+        }        
+        return true;
+    }
 }
