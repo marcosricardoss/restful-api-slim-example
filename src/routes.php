08fca989a48f953bc4458f77960cfdaf0d51a347
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-28 23:03:29 -0300

    [Chore] Added a route for creating of posts

--- src/routes.php
+++ src/routes.php
@@ -10,6 +10,10 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+$app->group('/posts', function () {
+    $this->map(['POST'], '', "Marcosricardoss\Restful\Controller\PostController:create");    
+})->add("Marcosricardoss\Restful\Middleware\AuthMiddleware");
+
 $app->group('/auth', function () {
     $this->post('/login', "Marcosricardoss\Restful\Controller\AuthController:login");
     $this->post('/register', "Marcosricardoss\Restful\Controller\AuthController:register");    
