8eeeb43b5307fad1ce460eeb3dd9ad7445a10b42
Marcos Ricardo <marcosricardoss@gmail.com>
2018-03-28 21:47:23 -0300

    [Chore] Added in the User model, the method to get the user posts

--- src/Model/User.php
+++ src/Model/User.php
@@ -8,12 +8,22 @@ class User extends Eloquent {
     
     protected $fillable = ['username', 'password', 'role'];
 
+    /**
+     * Get the black list tokens of the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasMany
+     */
     public function blacklistedTokens() {
         return $this->hasMany("Marcosricardoss\Restful\Model\BlacklistedToken", 'user_id');
     }
 
+    /**
+     * Get the posts of the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasMany
+     */
     public function posts() {
-        return;
+        return $this->hasMany('Marcosricardoss\Restful\Model\Post', 'created_by');
     }
 
 }
